cafa58de9714e38db4cf8a762a2e5e570e1f4f81
Vivek Verma <vermavivek334@gmail.com>
2023-05-03 16:03:04 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const boxesContainer = document.getElementById('boxes')
+const btn = document.getElementById('btn')
+var x = document.getElementById("myAudio"); 
+
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+function createBoxes() {
+    
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            const box = document.createElement('div')
+            box.classList.add('box')
+            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+            boxesContainer.appendChild(box)
+        }
+    }
+}
+
+
+createBoxes()
+
+
+function playAudio() { 
+    x.play(); 
+  } 
